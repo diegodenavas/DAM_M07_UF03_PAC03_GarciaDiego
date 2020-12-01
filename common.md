43307dd3650cd6147df638fa793918abc459c997
Javier Noguera <jnoguera@gmail.com>
2020-12-01 22:00:49 +0100

    Puntos de control inicial

---
+++ common.md
@@ -0,0 +1,26 @@
+## Cosas comunes
+
+Se debe evitar el spanglish. Esto significa que debes de usar permanente el ingles.
+ * Nombre de ficheros
+ * Nombre de funciones
+ * Nombre de las variables
+ ....
+
+ ## Patron de diseño MVC
+
+ Este patron de diseño consiste en:
+
+ * Modelo
+ * Vista 
+ * Controlador
+
+ ¿Que piensas que significa y que tiene que hacer cada una de estas capas?
+
+
+ ## Clean code
+
+ ¿Que concepto tienes de clean code? ¿Que consideras que son buenas practicas a la hora de escribir codigo?
+
+ ## SOLID
+
+ ¿Que significa y que implica implementar este tipo de "soluciones"?
--- controllers/actualizar_noticiaController.php
+++ controllers/actualizar_noticiaController.php
@@ -2,14 +2,18 @@
 
 require("../models/funciones_bd.php");
 
+// Porque definimos variables que pueden ser usadas directamente ?? que nos aporta tenerlas definidas
 $id = $_POST["id"];
 $titulo = $_POST["titulo"];
 $contenido = $_POST["contenido"];
 $autor = $_POST["autor"];
 $hora_creacion = $_POST["hora_creacion"];
 
+// Que nos aporta tener un array sin control de datos????
 $arrayDatos = array($titulo, $contenido, $autor, $hora_creacion);
 
+// Porque llamamos controller algo q es un new de la clase Funciones_bd ??
+// Se debe evitar el uso de snake case para la nomenclatura
 $controller = new Funciones_bd();
 
 $respuesta = $controller->actualizarNoticia($arrayDatos, $id);
